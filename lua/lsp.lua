1e43a47561a3093aca331f08cb9adf8d8270c9e8
Alexander Davidson Bryan <alxdb@pm.me>
2023-04-04 19:47:45 +0100

    use default lua lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -31,7 +31,7 @@ require('lsp-format').setup {}
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lspconfig = require('lspconfig')
-lspconfig.sumneko_lua.setup {
+lspconfig.lua_ls.setup {
   on_attach = on_attach,
   settings = {
     Lua = {
