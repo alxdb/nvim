22efc94f0c323895e1a712e0df45ea61910cf291
Alexander Davidson Bryan <alxdb@pm.me>
2023-08-22 20:16:04 +0100

    initial commit
    Added rust support (with debugging)

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,5 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>cx", vim.lsp.codelens.run, { desc = "Run the codelens in the current line" })
