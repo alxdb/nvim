5e9d61aac9e3d9a5fc8684639f5d38b9ac8047b6
Alexander Davidson Bryan <alxdb@pm.me>
2023-09-26 23:38:26 +0100

    use telescopes vertical layout by default

--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -1,3 +1,4 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", opts = { filesystem = { scan_mode = "deep", group_empty_dirs = true } } },
+  { "nvim-telescope/telescope.nvim", opts = { defaults = { layout_strategy = "vertical" } } },
 }
